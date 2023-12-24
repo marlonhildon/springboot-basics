b1043792a3a4814a788a40c1a8209a9c2d2a0533
Marlon Hildon <marlonhildon@gmail.com>
2023-12-23 21:34:46 -0300

    first commit

---
+++ src/test/java/br/com/marlonhildon/springbootbasics/SpringbootBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonhildon.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
